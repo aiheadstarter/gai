9e555d32ad13e79fbb53156ff1c37159d056df3e
sjkim114 <sjkim114@cau.ac.kr>
2023-04-23 16:05:19 +0900

    second commit

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "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"
+API_KEY = "0000000000"
 API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
 def chatgpt_response(prompt):
@@ -36,3 +36,5 @@ def chatbot():
 
 if __name__ == "__main__":
     chatbot()
+    
+    print(test)
